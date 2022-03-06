5a6622a92e88a041e4dc903ac4b397f6bf4bac0d
pratikm0007 <pratikpmondal07@gmail.com>
2022-03-06 16:20:10 +0530

    push UC1 file

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EmployWageComputation
+{
+    class Program
+    {
+        /// <summary>
+        /// Check employs are absent or present.
+        /// </summary>
+        static void Main(string[] args)
+        {
+
+            Random random = new Random();
+            int presenty = random.Next(2);
+
+            if (presenty == 1)
+            {
+                Console.WriteLine("Employ is Present");
+
+            }
+            else if (presenty == 0)
+            {
+                Console.WriteLine("Employ is Absent");
+                Console.WriteLine("Employ per Day Wage is : 0");
+            }
+        }
+    }
+}
