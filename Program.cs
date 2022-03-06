5e3c7730037467bcf51e069279604cd8a73147f9
pratikm0007 <pratikpmondal07@gmail.com>
2022-03-06 17:47:07 +0530

    Push UC5 file

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace EmployWageComputation
+{
+    class Program
+    {
+        /// <summary>
+        /// Monthly wage
+        /// </summary>
+        static void Main(string[] args)
+        {
+            int employWage = 0;
+            int presentHours = 0;
+            int monthlyEmpWage = 0;
+            const int empWagePerHour = 20;
+            const int dayInMonth = 20;
+
+            for (int i = 0; i < dayInMonth; i++)
+            {
+                Random random = new Random();
+                int presenty = random.Next(3);
+
+                switch (presenty)
+                {
+                    case 0:
+                        Console.WriteLine("Employ is Full Time Present");
+                        presentHours = 8;
+                        break;
+                    case 1:
+                        Console.WriteLine("Employ is Part Time Present");
+                        presentHours = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Employ is Absent");
+                        presentHours = 0;
+                        break;
+                }
+                employWage = empWagePerHour * presentHours;
+                monthlyEmpWage += employWage;
+                Console.WriteLine("Employ wage is : {0}", + employWage);
+            }
+            Console.WriteLine("Monthly Employ wage is :{0}",+ monthlyEmpWage);
+            
+        }        
+    }
+}
